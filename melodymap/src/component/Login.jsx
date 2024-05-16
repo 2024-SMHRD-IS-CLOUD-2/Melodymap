63d7c242ab5bd54b91b67d15b76818f30edd1993
김강운임 <pa375744@gmail.com>
2024-05-16 12:23:43 +0900

    로그인,회원가입 axios
    로그인,회원가입 axios

--- melodymap/src/component/Login.jsx
+++ melodymap/src/component/Login.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../css/login.css";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [userID, setUserID] = useState("");
+  const [userPW, setUserPW] = useState("");
+
+  const loginForm = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8081/api/login",
+        {
+          userID,
+          userPW,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.data === "Login successful") {
+        // 로그인 성공 시 수행할 동작
+        navigate("/"); // 예시로 메인 페이지로 이동
+      } else {
+        // 로그인 실패 시 수행할 동작
+        alert("로그인 실패: " + response.data);
+      }
+    } catch (error) {
+      console.error("로그인 중 오류 발생:", error);
+      alert("로그인 중 오류가 발생했습니다.");
+    }
+  };
+
   return (
     <div className="container5">
       <div className="wrapper5">
@@ -13,18 +44,23 @@ const Login = () => {
             className="input15"
             type="text"
             placeholder="ID를 입력해주세요"
+            value={userID}
+            onChange={(e) => setUserID(e.target.value)}
           />
           <br />
           <input
             className="input25"
-            type="text"
+            type="password"
             placeholder="PW를 입력해주세요"
+            value={userPW}
+            onChange={(e) => setUserPW(e.target.value)}
           />
 
           <div className="socialLogin5">
             <a
               href="https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/"
               target="_blank"
+              rel="noopener noreferrer"
             >
               <img
                 className="naver5"
@@ -35,6 +71,7 @@ const Login = () => {
             <a
               href="https://accounts.kakao.com/login/?continue=https%3A%2F%2Flogins.daum.net%2Faccounts%2Fksso.do%3Frescue%3Dtrue%26url%3Dhttps%253A%252F%252Fwww.daum.net#login"
               target="_blank"
+              rel="noopener noreferrer"
             >
               <img
                 className="kakao5"
@@ -44,15 +81,12 @@ const Login = () => {
             </a>
           </div>
           <div>
-            <button
-              onClick={() => {
-                navigate("/Join");
-              }}
-              className="button15"
-            >
+            <button className="button15" onClick={() => navigate("/Join")}>
               회원가입
             </button>
-            <button className="button25">로그인</button>
+            <button className="button25" onClick={loginForm}>
+              로그인
+            </button>
           </div>
         </div>
       </div>
